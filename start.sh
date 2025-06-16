724f1be6c2352c5316b5e419ca1c1891d4096fe2
Nolan1038 <90103911+Nolan1038@users.noreply.github.com>
2025-06-15 20:03:53 -0500

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# start.sh - script to start your Discord bot on Replit
+
+# Install dependencies (optional, Replit does this automatically but doesn't hurt)
+pip install -r requirements.txt
+
+# Run your bot
+python3 tracker.py
